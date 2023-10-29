f3f556e8fcf2425c0d4a0eba9a7267fb4d3791bc
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-10-29 01:04:15 +0100

    trans

--- transactions.js
+++ transactions.js
@@ -1,24 +1,30 @@
 document.cookie = "userid=70560991";
+$(document).ready( function() {
 
-var req = $.ajax({
-    type: "GET",
-    url: "/getTransactions.php",
-    async: false,
+    var req = $.ajax({
+        type: "GET",
+        url: "/getTransactions.php",
+        async: false,
+    });
+
+    var responce = JSON.parse(req.responseText);
+
+    var list = document.getElementById("transactions-list");
+    var transactions = responce.Transactions;
+
+    for (var i in transactions) {
+        var transaction = transactions[i]
+        console.log(transaction)
+        var div = document.createElement('div');
+        div.innerHTML = '<a href="#" class="list-group-item list-group-item-action" aria-current="true"> \
+                        <div class="d-flex w-100 justify-content-between"> \
+                        <h5 class="mb-1">'+transaction.merchant.name+'</h5> \
+                        <small>£'+transaction.amount+'</small> \
+                        </div> \
+                        <p class="mb-1">'+transaction.timestamp+'</p> \
+                        <small>'+transaction.message+'</small> \
+                        </a>';
+    
+        list.appendChild(div);
+    }
 });
-
-var responce = JSON.parse(req.responseText);
-
-var list = document.getElementById("transactions-list");
-var transactions = responce.transactions
-
-var div = document.createElement('div');
-div.innerHTML = '<a href="#" class="list-group-item list-group-item-action" aria-current="true"> \
-                <div class="d-flex w-100 justify-content-between"> \
-                <h5 class="mb-1">Transaction</h5> \
-                <small>£100</small> \
-                </div> \
-                <p class="mb-1">lorum ipsum</p> \
-                <small>its all fake</small> \
-                </a>';
-
-list.appendChild(div)
