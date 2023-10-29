084a27f872803485af9c4be1056511c494798849
Thomas Leitch <thomas.leitch@student.manchester.ac.uk>
2023-10-29 02:27:58 +0000

    trans2

--- transactions.js
+++ transactions.js
@@ -1,5 +1,9 @@
-document.cookie = "userid=70560991";
+var transactions, dateOrder, amountOrder;
+var listDiv;
+
 $(document).ready( function() {
+
+    document.cookie = "userid=70560991";
 
     var req = $.ajax({
         type: "GET",
@@ -9,22 +13,76 @@ $(document).ready( function() {
 
     var responce = JSON.parse(req.responseText);
 
-    var list = document.getElementById("transactions-list");
-    var transactions = responce.Transactions;
+    listDiv = document.getElementById("transactions-list");
+    transactions = responce.Transactions;
+    console.log(transactions);
+    dateOrder = bubbleSort(transactions, true);
+    console.log(transactions, dateOrder);
+    amountOrder = bubbleSort(transactions, false);
+    console.log(dateOrder, amountOrder);
 
-    for (var i in transactions) {
-        var transaction = transactions[i]
-        console.log(transaction)
-        var div = document.createElement('div');
-        div.innerHTML = '<a href="#" class="list-group-item list-group-item-action" aria-current="true"> \
-                        <div class="d-flex w-100 justify-content-between"> \
-                        <h5 class="mb-1">'+transaction.merchant.name+'</h5> \
-                        <small>£'+transaction.amount+'</small> \
-                        </div> \
-                        <p class="mb-1">'+transaction.timestamp+'</p> \
-                        <small>'+transaction.message+'</small> \
-                        </a>';
-    
-        list.appendChild(div);
+    for (var i in dateOrder) {
+        listDiv.appendChild(createTransaction(transactions[dateOrder[dateOrder.length-i-1]]));
     }
 });
+
+function order(value){
+    console.log("order", value)
+    switch(value) {
+        case "1":
+            console.log(listDiv)
+            listDiv.innerHTML = "";
+            for (var i in amountOrder) {
+                listDiv.appendChild(createTransaction(transactions[amountOrder[i]]));
+            }
+            break;
+        case "0":
+            listDiv.innerHTML = "";
+            for (var i in amountOrder) {
+                listDiv.appendChild(createTransaction(transactions[amountOrder[amountOrder.length-i-1]]));
+            }
+            break;
+        case "3":
+            listDiv.innerHTML = "";
+            for (var i in dateOrder) {
+                listDiv.appendChild(createTransaction(transactions[dateOrder[i]]));
+            }
+            break;
+        case "2":
+            listDiv.innerHTML = "";
+            for (var i in dateOrder) {
+                listDiv.appendChild(createTransaction(transactions[dateOrder[dateOrder.length-i-1]]));
+            }
+            break;
+    }
+}
+
+function bubbleSort(arr, date) { 
+    var indexes = Array(arr.length).fill().map((x,i)=>i)
+    for (var i = 0; i < arr.length; i++) { 
+          for (var j = 0; j < (arr.length - i - 1); j++) { 
+            val1 = (date) ? Date.parse(arr[indexes[j]].timestamp) : arr[indexes[j]].amount;
+            val2 = (date) ? Date.parse(arr[indexes[j+1]].timestamp) : arr[indexes[j+1]].amount;
+            if (val1 > val2) { 
+                var temp = indexes[j];
+                indexes[j] = indexes[j + 1]; 
+                indexes[j + 1] = temp; 
+            } 
+        } 
+    } 
+    return indexes; 
+} 
+
+function createTransaction(transaction) {
+    var div = document.createElement('div');
+    div.innerHTML = '<a href="#" class="list-group-item list-group-item-action" aria-current="true"> \
+                    <div class="d-flex w-100 justify-content-between"> \
+                    <h5 class="mb-1">'+transaction.merchant.name+' '+transaction.emoji+'</h5> \
+                    <small>'+transaction.timestamp+'</small> \
+                    </div> \
+                    <p class="mb-1">£'+transaction.amount+'</p> \
+                    <small>'+transaction.message+'</small> \
+                    </a>';
+    return div;
+}
+ 
